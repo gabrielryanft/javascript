60c18e37c2dddc86d53e09475fe33e21bf1cf2be
Gabriel Ryan <135415258+gabrielryanft@users.noreply.github.com>
2023-11-23 06:03:31 -0300

    Create calculadoradenota.html

---
+++ calculadoradenota/script.js
@@ -0,0 +1,27 @@
+//modal: 
+
+const calculo_explicacao = document.getElementById("calculo_explicacao")
+aparecendo_explicacao = 0
+
+function abrir(){
+    calculo_explicacao.showModal()
+    aparecendo_explicacao = 1
+}
+
+function fechar(){
+    calculo_explicacao.close()
+    aparecendo_explicacao = 0
+}
+
+//cálculo: 
+
+function calcular(){
+    var nota_primeiro = document.getElementById("nota1trimestre").value
+    var nota_segundo = document.getElementById("nota2trimestre").value
+    var nota_passar = document.getElementById("nota_passar")
+
+    var nota_terceiro = (60 - ((3 * nota_primeiro) + (3 * nota_segundo))) / 4
+
+    nota_passar.innerText = nota_terceiro
+}
+    
