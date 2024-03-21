465eff3d69739dbb1ae238787dd4130ce52dbf05
Gabriel Ryan <135415258+gabrielryanft@users.noreply.github.com>
2024-03-21 07:00:12 -0300

    Updated dividindo_igualmente

--- dividido_igualmente/script.js
+++ dividido_igualmente/script.js
@@ -1,36 +1,60 @@
 const img = document.querySelector("img")
+const wrapper = document.querySelector("#wrapper")
 const img_user = document.querySelector("#img_user")
-img_user.addEventListener("change", (e)=>{
+
+img.onload = function(){
+    if(img.height > img.width){
+        wrapper.style.height = `${img_sz.value}%`
+        tamanho.textContent = `h${img_sz.value}`
+        wrapper.style.width = "auto"
+    } else{
+        wrapper.style.width = `${img_sz.value}%`
+        tamanho.textContent = `w${img_sz.value}`
+        wrapper.style.height = "auto"
+    }
+}
+img_user.addEventListener("change", ()=>{
     let reader = new FileReader()
     reader.onload = function(){
         img.src = reader.result
     }
-    reader.readAsDataURL(img_user.files[0])
-
+    img.onload = function(){
+        if(img.height > img.width){
+            wrapper.style.height = `${img_sz.value}%`
+            tamanho.textContent = `h${img_sz.value}`
+            wrapper.style.width = "auto"
+        } else{
+            wrapper.style.width = `${img_sz.value}%`
+            tamanho.textContent = `w${img_sz.value}`
+            wrapper.style.height = "auto"
+        }
+    }
+    reader.readAsDataURL(img.src)
 })
+
 const link_img = document.querySelector("#link_img")
 function submit_img(){
     img.src = link_img.value
 }
+
 const img_sz = document.querySelector("#img_sz")
 var tamanho = document.querySelector("#tamanho")
-img_sz.addEventListener("change", ()=>{
-
+img_sz.addEventListener("input", ()=>{
     if(img.height > img.width){
-        img.style.height = `${img_sz.value}%`
+        wrapper.style.height = `${img_sz.value}%`
         tamanho.textContent = `h${img_sz.value}`
-        img.style.width = "auto"
+        wrapper.style.width = "auto"
     } else{
-        img.style.width = `${img_sz.value}%`
+        wrapper.style.width = `${img_sz.value}%`
         tamanho.textContent = `w${img_sz.value}`
-        img.style.height = "auto"
+        wrapper.style.height = "auto"
     }
 })
-// const num_col = document.querySelector("#num_col")
-// num_col.addEventListener("change", ()=>{
-    
-// })
-// const num_row = document.querySelector("#num_row")
-// num_row.addEventListener("change", ()=>{
-
-// })
+const num_col = document.querySelector("#num_col")
+num_col.addEventListener("change", ()=>{
+    document.documentElement.style.setProperty("--n_col", `${100 / num_col.value}%`)
+})
+const num_row = document.querySelector("#num_row")
+num_row.addEventListener("change", ()=>{
+    document.documentElement.style.setProperty("--n_row", `${100 / num_row.value}%`)
+})
