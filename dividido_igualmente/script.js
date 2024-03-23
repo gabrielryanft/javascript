70c4c38339da5eadc10a757bf4120f293a3c366e
Gabriel Ryan <135415258+gabrielryanft@users.noreply.github.com>
2024-03-23 10:22:31 -0300

    Fixed problem in dividido_igualmente
    Fixed the image size problem.

--- dividido_igualmente/script.js
+++ dividido_igualmente/script.js
@@ -2,59 +2,62 @@ const img = document.querySelector("img")
 const wrapper = document.querySelector("#wrapper")
 const img_user = document.querySelector("#img_user")
 
-img.onload = function(){
-    if(img.height > img.width){
-        wrapper.style.height = `${img_sz.value}%`
-        tamanho.textContent = `h${img_sz.value}`
-        wrapper.style.width = "auto"
-    } else{
-        wrapper.style.width = `${img_sz.value}%`
-        tamanho.textContent = `w${img_sz.value}`
-        wrapper.style.height = "auto"
-    }
-}
-img_user.addEventListener("change", ()=>{
-    let reader = new FileReader()
-    reader.onload = function(){
+img_user.addEventListener("change", () => {
+    const reader = new FileReader()
+    reader.addEventListener("load", () => {
         img.src = reader.result
-    }
-    img.onload = function(){
-        if(img.height > img.width){
+        console.log(img.width/img.height)
+        console.log(img.height/img.width)
+        if ((img.width/img.height) <= 1.63636363636) {
+            wrapper.style.height = `${img_sz.value}%`
+            // wrapper.style.height = `calc(11vmin * 5)`
+            tamanho.textContent = `h${img_sz.value}`
+            wrapper.style.width = "auto"
+        } else if((img.width/img.height) > 1.63636363636){
+            wrapper.style.width = `${img_sz.value}%`
+            // wrapper.style.width = `calc(18vmin * 5)`
+            tamanho.textContent = `w${img_sz.value}`
+            wrapper.style.height = "auto"
+        }
+    })
+    reader.readAsDataURL(img_user.files[0])
+})
+
+const link_img = document.querySelector("#link_img")
+function submit_img() {
+    img.src = link_img.value
+    img.addEventListener("load", ()=>{
+        if (img.height > img.width) {
             wrapper.style.height = `${img_sz.value}%`
             tamanho.textContent = `h${img_sz.value}`
             wrapper.style.width = "auto"
-        } else{
+        } else {
             wrapper.style.width = `${img_sz.value}%`
             tamanho.textContent = `w${img_sz.value}`
             wrapper.style.height = "auto"
         }
-    }
-    reader.readAsDataURL(img.src)
-})
-
-const link_img = document.querySelector("#link_img")
-function submit_img(){
-    img.src = link_img.value
+    })
 }
 
 const img_sz = document.querySelector("#img_sz")
 var tamanho = document.querySelector("#tamanho")
-img_sz.addEventListener("input", ()=>{
-    if(img.height > img.width){
+img_sz.addEventListener("input", () => {
+    if (img.height > img.width) {
         wrapper.style.height = `${img_sz.value}%`
         tamanho.textContent = `h${img_sz.value}`
         wrapper.style.width = "auto"
-    } else{
+    } else {
         wrapper.style.width = `${img_sz.value}%`
         tamanho.textContent = `w${img_sz.value}`
         wrapper.style.height = "auto"
     }
 })
 const num_col = document.querySelector("#num_col")
-num_col.addEventListener("change", ()=>{
+function num_col_change(){
     document.documentElement.style.setProperty("--n_col", `${100 / num_col.value}%`)
-})
+}
+
 const num_row = document.querySelector("#num_row")
-num_row.addEventListener("change", ()=>{
+function num_row_change(){
     document.documentElement.style.setProperty("--n_row", `${100 / num_row.value}%`)
-})
+}
