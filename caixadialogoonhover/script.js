53e556b816a6555f2d06222df421b42dd0ea5377
Gabriel Ryan <135415258+gabrielryanft@users.noreply.github.com>
2024-07-07 18:04:20 -0300

    fix unsplash pic and footer animation.

--- caixadialogoonhover/script.js
+++ caixadialogoonhover/script.js
@@ -14,8 +14,10 @@ function sudoMouse_position() {
             document.documentElement.style.setProperty(`--y_mouse${index}`, `${y}px`)
 
             if(index==3){
-                document.documentElement.style.setProperty("--min_wh", "40vmin")
-                document.documentElement.style.setProperty("--max_wh", "50vmin")
+                document.documentElement.style.setProperty("--min_w", "35vmin")
+                document.documentElement.style.setProperty("--min_h", "21vmin")
+                document.documentElement.style.setProperty("--max_w", "35vmin")
+                document.documentElement.style.setProperty("--max_h", "21vmin")
                 document.documentElement.style.setProperty("--outln_sz", "8px")
                 document.documentElement.style.setProperty("--outln_clr", "rgba(0, 33, 0, 0.838)")
             } else {
@@ -26,8 +28,10 @@ function sudoMouse_position() {
         img.addEventListener("mouseout", () => {
             setTimeout(() => {
                 if(index==3){
-                    document.documentElement.style.setProperty("--min_wh", "0")
-                    document.documentElement.style.setProperty("--max_wh", "0")
+                    document.documentElement.style.setProperty("--min_w", "0")
+                    document.documentElement.style.setProperty("--min_h", "0")
+                    document.documentElement.style.setProperty("--max_w", "0")
+                    document.documentElement.style.setProperty("--max_h", "0")
                     document.documentElement.style.setProperty("--outln_sz", "0")
                     document.documentElement.style.setProperty("--outln_clr", "transparent")
                 } else {
