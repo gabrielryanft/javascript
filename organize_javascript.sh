9ed9f17039a0990265e47fef1c81c39d5efa9fcd
gabrielryanft <135415258+gabrielryanft@users.noreply.github.com>
2024-11-20 20:55:50 -0300

    Create README.md and update organize_javascript.sh
    Create README.md;
    Update organize_javascript.sh to use temp files.

--- organize_javascript.sh
+++ organize_javascript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ls | grep -v -e "README.md" -e "LICENSE" -e "organize_javascript.sh" -e "exists.txt" > exists.txt
+tmp_file=$(mktemp)
+ls | grep -v -e "README.md" -e "LICENSE" -e "organize_javascript.sh" -e "exists.txt" > $tmp_file
 printf "# JavaScript\n" > README.md
 while read name
 do
 	printf "<a href='https://gabrielryanft.github.io/javascript/$name/index.html' target='_blank' rel='next'>$name</a><br/>\n" >> README.md
 
-done < exists.txt
+done < $tmp_file
--- sc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash	
-for folder in ./*
-do 
-	for name in ./$folder/*.html
-	do 
-		mv ./$folder/*.html ./$folder/index.html
-	done
-done
