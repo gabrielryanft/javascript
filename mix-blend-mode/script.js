0bd11dfec18d62be99de411348bb4eee30ab2c8b
gabrielryanft <135415258+gabrielryanft@users.noreply.github.com>
2024-12-16 14:10:44 -0300

    update image in style.css

--- mix-blend-mode/script.js
+++ mix-blend-mode/script.js
@@ -1,25 +1,25 @@
-const c = document.querySelectorAll("canvas")
+const c = document.querySelectorAll("canvas");
 
-var cc = []
+var cc = [];
 c.forEach((canvas) => {
 	// Canvas setup
-	cc.push(canvas.getContext("2d"))
-	canvas.width = canvas.offsetWidth
-	canvas.height = canvas.offsetHeight
-})
+	cc.push(canvas.getContext("2d"));
+	canvas.width = canvas.offsetWidth;
+	canvas.height = canvas.offsetHeight;
+});
 
-var particles = []
+var particles = [];
 var mouse_pos = {
 	x: undefined,
 	y: undefined,
-}
+};
 
 class Particle {
 	constructor() {
-		this.x = mouse_pos.x
-		this.y = mouse_pos.y
-		this.color = "rgba(100, 0, 0, 0.5)"
-		this.size = 10
+		this.x = mouse_pos.x;
+		this.y = mouse_pos.y;
+		this.color = "rgba(100, 0, 0, 0.5)";
+		this.size = 10;
 	}
 
 	draw() {
@@ -33,49 +33,54 @@ class Particle {
 
 		// Draw a square in all the canvas.
 		cc.forEach((canvas) => {
-			canvas.beginPath()
-			canvas.fillStyle = this.color
-			canvas.rect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size)
-			canvas.fill()
-			canvas.closePath()
-		})
+			canvas.beginPath();
+			canvas.fillStyle = this.color;
+			canvas.rect(
+				this.x - this.size / 2,
+				this.y - this.size / 2,
+				this.size,
+				this.size,
+			);
+			canvas.fill();
+			canvas.closePath();
+		});
 	}
 }
 
-var mouse_down = false
-var canvas_i
+var mouse_down = false;
+var canvas_i;
 
 c.forEach((canvas, index) => {
 	canvas.addEventListener("pointerdown", (event) => {
-		event.preventDefault()
-		mouse_down = true
-		canvas_i = index
+		event.preventDefault();
+		mouse_down = true;
+		canvas_i = index;
 
-		const rect = canvas.getBoundingClientRect()
-		mouse_pos.x = event.clientX - rect.left
-		mouse_pos.y = event.clientY - rect.top
+		const rect = canvas.getBoundingClientRect();
+		mouse_pos.x = event.clientX - rect.left;
+		mouse_pos.y = event.clientY - rect.top;
 
-		particles.push(new Particle())
-		particles[particles.length - 1].draw()
-	})
+		particles.push(new Particle());
+		particles[particles.length - 1].draw();
+	});
 
 	canvas.addEventListener("pointermove", (event) => {
 		if (mouse_down) {
-			const rect = canvas.getBoundingClientRect()
-			mouse_pos.x = event.clientX - rect.left
-			mouse_pos.y = event.clientY - rect.top
+			const rect = canvas.getBoundingClientRect();
+			mouse_pos.x = event.clientX - rect.left;
+			mouse_pos.y = event.clientY - rect.top;
 
-			particles.push(new Particle())
-			particles[particles.length - 1].draw()
+			particles.push(new Particle());
+			particles[particles.length - 1].draw();
 		}
-	})
+	});
 
 	canvas.addEventListener("pointerup", () => {
-		mouse_down = false
-	})
+		mouse_down = false;
+	});
 
 	// Stop drawing on mouse out
 	//canvas.addEventListener("mouseout", () => {
 	//	mouse_down = false // Stop drawing
 	//})
-})
+});
