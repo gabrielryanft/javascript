3b731cc93e25dd8c551a7de5a125e19631fe7946
Gabriel Ryan <gabrielryanf.t@gmail.com>
2025-06-13 12:58:08 -0300

    make footer animation of mix-blend-mode and info card

--- mix-blend-mode/script.js
+++ mix-blend-mode/script.js
@@ -173,3 +173,16 @@ showname_y.addEventListener("input", () => {
 		});
 	}
 });
+
+const modal = document.querySelector("#modal")
+const abrir_modal = document.querySelector("#abrir_modal")
+const fechar_modal = document.querySelector("#fechar_modal")
+
+abrir_modal.addEventListener("click", () =>{
+    modal.showModal()
+    console.log("Modal aberto!")
+})
+fechar_modal.addEventListener("click", () => {
+    modal.close()
+    console.log("Modal fechado!")
+})
